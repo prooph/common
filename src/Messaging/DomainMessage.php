c027c37c2d6260836639054f2101442007680c01
Fritz Gerneth <gerneth.fritz@funct.com>
2020-12-16 16:45:33 +0100

    Fix License year

--- src/Messaging/DomainMessage.php
+++ src/Messaging/DomainMessage.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of prooph/common.
  * (c) 2014-2020 Alexander Miertsch <kontakt@codeliner.ws>
- * (c) 2014-2020 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * (c) 2015-2020 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
